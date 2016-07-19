e205195f7f82dfeeb4639f9ffc26e07906292c5b
Ohad Levy <ohadlevy@gmail.com>
2016-07-19 18:32:51 +0300

    initial rex support

---
+++ proxy/Gemfile.local.rb
@@ -0,0 +1,3 @@
+gem 'smart_proxy_dynflow', :github => 'theforeman/smart_proxy_dynflow' # until a new version is released to include https://github.com/theforeman/smart_proxy_dynflow/pull/24/
+gem 'smart_proxy_dynflow_core', :github => 'theforeman/smart_proxy_dynflow' # until a new version is released to include https://github.com/theforeman/smart_proxy_dynflow/pull/24/
+gem 'smart_proxy_remote_execution_ssh', :github => 'theforeman/smart_proxy_remote_execution_ssh'
