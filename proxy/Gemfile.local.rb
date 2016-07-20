91be56eb4a4bcf5a77edc4f547c603f3f7a392f4
Ohad Levy <ohadlevy@gmail.com>
2016-07-20 16:22:00 +0300

    added missing ssh core gem.

--- proxy/Gemfile.local.rb
+++ proxy/Gemfile.local.rb
@@ -1,3 +1,4 @@
-gem 'smart_proxy_dynflow', :github => 'theforeman/smart_proxy_dynflow' # until a new version is released to include https://github.com/theforeman/smart_proxy_dynflow/pull/24/
-gem 'smart_proxy_dynflow_core', :github => 'theforeman/smart_proxy_dynflow' # until a new version is released to include https://github.com/theforeman/smart_proxy_dynflow/pull/24/
+gem 'smart_proxy_dynflow', :github => 'theforeman/smart_proxy_dynflow'
+gem 'smart_proxy_dynflow_core', :github => 'theforeman/smart_proxy_dynflow'
+gem 'smart_proxy_remote_execution_ssh_core', :github => 'theforeman/smart_proxy_remote_execution_ssh'
 gem 'smart_proxy_remote_execution_ssh', :github => 'theforeman/smart_proxy_remote_execution_ssh'
